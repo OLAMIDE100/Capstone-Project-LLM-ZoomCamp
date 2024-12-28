c62c18706487fe9abaaa524d3fe6bd12a4e8b164
Adewale Olamide Adesoba <adebimpe@Adewales-MBP.lan>
2024-12-28 21:36:14 +0200

    added repository secret

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+
+export TF_VAR_image_tag=${GITHUB_RUN_NUMBER}-${GITHUB_RUN_ATTEMPT}
+export TF_VAR_db_password=$(aws secretsmanager get-secret-value --secret-id postgres-secret   --query SecretString --output text |  jq -r '."postgrespassword"')
+
+
+
+echo 'yes' | terraform -chdir=infrastructure/terraform init  -backend-config="./backend.config"
+echo 'yes' | terraform -chdir=infrastructure/terraform plan 
+echo 'yes' | terraform -chdir=infrastructure/terraform apply 
+
+
+
+hostname=$(aws rds describe-db-instances --db-instance-identifier llm-eks  --query "DBInstances[0].Endpoint.Address" --output text)
+
+aws secretsmanager put-secret-value --secret-id postgres-secret --secret-string "{\"hostname\": \"${hostname}\",\"postgrespassword\": \"${TF_VAR_db_password}\"}"
